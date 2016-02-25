fa43bb8e44a36a5b505698f44c2222cf5c3862fc
teknikpata <patrik673@comhem.se>
2016-02-25 21:25:20 +0100

    Repeating clouds update(IN PROGRESS)
    uppdatering utav molnen

--- ExempleScene v0.1/Assets/Scripts/RepeatingClouds.cs
+++ ExempleScene v0.1/Assets/Scripts/RepeatingClouds.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RepeatingClouds : MonoBehaviour {
+    public Vector2 maxPosition;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private List<Transform> clouds; 
+
+    void Start() {
+        clouds = new List<Transform>();
+        foreach (Transform child in transform) {
+            clouds.Add(child);
+        }
+    }
+
+    void FixedUpdate() {
+        foreach (Transform cloud in clouds) {
+            cloud.transform.position = new Vector3(cloud.transform.position.x + 0.5f, cloud.transform.position.y, cloud.transform.position.z);
+        }
+    }
+
+
 }
