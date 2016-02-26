2b365af40f1507ce9f30378c0b70dde954ccdb91
Pothorian <pothorian@gmail.com>
2016-02-26 10:59:04 +0100

    Ficklampsuppdate
    Nyskapad ficklampa med realtidsljus. Påbörjat arbete med z-leds
    förflyttning

--- ExempleScene v0.1/Assets/Scripts/Flashlight/RealLightMovement.cs
+++ ExempleScene v0.1/Assets/Scripts/Flashlight/RealLightMovement.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class RealLightMovement : MonoBehaviour {
     public GameObject realLightSprite;
     public bool lightToggleCheck = false;
+    public Vector2 distanceModifier;
+    public float depthSpeed;
+    public Vector3 standardPosition;
+    float zMovement;
+    public GameObject Jack;
 
     void realLightDirection() {
         if (Camera.main != null) {
@@ -11,13 +16,26 @@ public class RealLightMovement : MonoBehaviour {
             Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
             transform.rotation = rot;
             transform.eulerAngles = new Vector3(-transform.eulerAngles.x, -transform.eulerAngles.y, 0);
-            float input = Input.GetAxis("Vertical");
         }
+    }
+
+    void realLightDepth() {
+        //if (Camera.main != null) {
+        //    Vector3 mousePositionRelative = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        //    if (mousePositionRelative.x > distanceModifier.x || -mousePositionRelative.x < -distanceModifier.x || mousePositionRelative.y > distanceModifier.y || -mousePositionRelative.y < distanceModifier.y) {
+        //        if (transform.position.z > standardPosition.z) {
+        //            transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y + 3.4f, transform.position.z + mousePositionRelative.z);
+        //        }
+        //    } else if (mousePositionRelative.x < distanceModifier.x || -mousePositionRelative.x > -distanceModifier.x || mousePositionRelative.y < distanceModifier.y || -mousePositionRelative.y > -distanceModifier.y) {
+        //        if (transform.position.z < standardPosition.z) {
+        //            transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y + 3.4f, transform.position.z - mousePositionRelative.z);
+        //        }
+        //    }
+        //}
     }
 
     void FixedUpdate() {
         realLightDirection();
+        realLightDepth();
     }
 }
-
-
