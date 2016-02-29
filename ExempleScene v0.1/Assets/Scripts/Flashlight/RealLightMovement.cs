7ba7219c268d6c6391c8aea1c8ff5a56ca05ff57
Pothorian <pothorian@gmail.com>
2016-02-29 19:20:16 +0100

    Ficklampshelvete
    Ficklampan har fått rörelsen fullt fungerande, det enda som saknas är
    mjuka övergångar.

--- ExempleScene v0.1/Assets/Scripts/Flashlight/RealLightMovement.cs
+++ ExempleScene v0.1/Assets/Scripts/Flashlight/RealLightMovement.cs
@@ -12,18 +12,23 @@ public class RealLightMovement : MonoBehaviour {
     void realLightDirection() {
         if (Camera.main != null) {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.down);
-            transform.localRotation = rot;
-            //transform.eulerAngles = new Vector3(-transform.eulerAngles.x, -transform.eulerAngles.y, 0);
-            ////transform.localRotation = Quaternion.LookRotation(mousePosition);
-            //////Plane playerPlane = new Plane(Vector3.up, transform.position);
-            //////Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //////float hitdist = 0.0f;
-            //////if (playerPlane.Raycast (ray, out hitdist)) {
-            //////    Vector3 targetPoint = ray.GetPoint(hitdist);
-            //////    Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-            //////    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-            //////}
+            Quaternion rotate = Quaternion.LookRotation(transform.position - mousePosition, Vector3.up);
+            transform.localRotation = rotate;
+            transform.eulerAngles = new Vector3(-transform.eulerAngles.x, -transform.eulerAngles.y, 0);
+            if (mousePosition.x > Jack.transform.position.x + offSet) {
+                Vector3 mousePositionTwo = new Vector3(mousePosition.x + 5, mousePosition.y, mousePosition.z);
+                Quaternion rotateTwo = Quaternion.LookRotation(transform.position - mousePositionTwo, Vector3.up);
+                Quaternion slerp = Quaternion.Slerp(rotate, rotateTwo, 5f);
+                transform.localRotation = slerp;
+                transform.eulerAngles = new Vector3(-transform.eulerAngles.x, -transform.eulerAngles.y, 0);
+            } 
+            else if (mousePosition.x < Jack.transform.position.x - offSet) {
+                Vector3 mousePositionThree = new Vector3(mousePosition.x - 5, mousePosition.y, mousePosition.z);
+                Quaternion rotateThree = Quaternion.LookRotation(transform.position - mousePositionThree, Vector3.up);
+                Quaternion slerpTwo = Quaternion.Slerp(rotate, rotateThree, 5f);
+                transform.localRotation = slerpTwo;
+                transform.eulerAngles = new Vector3(-transform.eulerAngles.x, -transform.eulerAngles.y, 0);
+            }
         }
     }
 
@@ -31,20 +36,21 @@ public class RealLightMovement : MonoBehaviour {
         if (Camera.main != null) {
             Vector3 mousePosition = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
             if (mousePosition.x > Jack.transform.position.x + offSet || mousePosition.x < Jack.transform.position.x - offSet) {
-                transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y, -13.4f + Mathf.Abs((Jack.transform.position.x - mousePosition.x)));
-                if (transform.position.z >= 0) {
-                    transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y, -0.10f);
+                transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y, -3.8f + Mathf.Abs(mousePosition.x));
+                if (transform.position.z >= -3.8f){
+                    transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y, -3.8f);
                 }
-            } 
-            else if (mousePosition.x < Jack.transform.position.x + offSet || -mousePosition.x > -Jack.transform.position.x - offSet) {
-                if (transform.position.z >= -6.7) {
+            }
+            else if (mousePosition.x < Jack.transform.position.x + offSet || mousePosition.x > -Jack.transform.position.x - offSet) {
+                transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y, -6.7f);
+                if (transform.position.z <= -6.7){
                     transform.position = new Vector3(Jack.transform.position.x, Jack.transform.position.y, -6.7f);
                 }
             }
         }
     }
 
-    void FixedUpdate() {
+    void Update() {
         realLightDirection();
         realLightDepth();
     }
