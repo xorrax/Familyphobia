96c1511639852b7ab24d898a7fc69d0e6a682a58
teknikpata <patrik673@comhem.se>
2016-03-01 07:41:58 +0100

    Dynamic Music Start up
    Added dynamic music script
    TODO: Fade music

---
+++ ExempleScene v0.1/Assets/Scripts/DynamicMusic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicMusic : MonoBehaviour {
+
+    public float distance;
+    public AudioClip newSong;
+    private PlayAudio playAudio;
+    private Transform player;
+    private float clipTime;
+
+
+	void Start () {
+        playAudio = GameObject.Find("Music").GetComponent<PlayAudio>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        clipTime = 0;
+    }
+
+	void Update () {
+        float distanceFromPlayer = Vector3.Distance(player.position, transform.position);
+        if (distanceFromPlayer <= distance) {
+            playAudio.changeAudio(newSong, clipTime);
+
+        } else if (playAudio.getCurrentClip() == newSong) {
+            clipTime = playAudio.getCurrentClipTime();
+            playAudio.changeAudio(playAudio.getLastClip(), playAudio.getLastClipTime());
+        }
+	}
+}
--- ExempleScene v0.1/Assets/Scripts/PlayAudio.cs
+++ ExempleScene v0.1/Assets/Scripts/PlayAudio.cs
@@ -2,13 +2,46 @@
 using System.Collections;
 
 public class PlayAudio : MonoBehaviour {
-    public AudioClip soundEffect;
-    AudioSource audioSource;
+    public AudioClip currentClip;
+    private AudioSource audioSource;
+    private AudioClip lastClip;
+    private float lastClipTime;
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
-        audioSource.clip = soundEffect;
+        audioSource.clip = currentClip;
+        lastClip = currentClip;
+        lastClipTime = 0;
         audioSource.Play();
+    }
+
+    public void changeAudio(AudioClip newAudio, float time) {
+        if (newAudio != audioSource.clip) {
+            lastClip = audioSource.clip;
+            lastClipTime = audioSource.time;
+
+            audioSource.Stop();
+
+            currentClip = newAudio;
+
+            audioSource.clip = newAudio;
+            audioSource.Play();
+            audioSource.time = time;
+        }
+    }
+
+    public AudioClip getCurrentClip() {
+        return audioSource.clip;
+    }
+    public AudioClip getLastClip() {
+        return lastClip;
+    }
+    public float getLastClipTime() {
+        return lastClipTime;
+    }
+
+    public float getCurrentClipTime() {
+        return audioSource.time;
     }
 
 }
