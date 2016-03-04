ce7614714b4c958d50b11322ab65748b998031fa
Pothorian <pothorian@gmail.com>
2016-03-04 08:33:37 +0100

    Scenljus
    Rummens ljus kommer styras av att materialet byts från diffuse till
    default. Denna kod gör det samt hanterar spritebyte vid tända lampor.

--- ExempleScene v0.1/Assets/Scripts/Effects/RoomLighter.cs
+++ ExempleScene v0.1/Assets/Scripts/Effects/RoomLighter.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 
 public class RoomLighter : MonoBehaviour {
-    //SpriteRenderer EntranceRenderer;
+    public Material defaultMaterial;
+    public Material diffuseMaterial;
     //SpriteRenderer LivingRenderer;
     //SpriteRenderer KitchenRenderer;
     //SpriteRenderer ÖvreRenderer;
    // // Entrance
     bool entranceLit = false;
+    SpriteRenderer EntranceRenderer;
+    SpriteRenderer EntranceDrawerRenderer;
     //public Sprite EntranceDark;
     //public Sprite EntranceLivingRoomLit;
     //public Sprite EntranceKitchenLit;
@@ -18,6 +21,9 @@ public class RoomLighter : MonoBehaviour {
     //public Sprite EntranceAllThreeLit;
 
    // // Living Room
+    SpriteRenderer livingCouchRenderer;
+    SpriteRenderer livingTableRenderer;
+    SpriteRenderer livingRenderer;
     bool livingLit = false;
     //public Sprite LivingRoomDark;
     //public Sprite LivingRoomEntranceLit;
@@ -32,16 +38,41 @@ public class RoomLighter : MonoBehaviour {
     //public Sprite ÖvreEntranceDark;
     //public Sprite ÖvreEntranceUndreLit;
 
+    void Start() {
+        EntranceRenderer = GameObject.Find("Entrance_Background").GetComponent<SpriteRenderer>();
+        EntranceDrawerRenderer = GameObject.Find("Entrance_Byra").GetComponent<SpriteRenderer>();
+        livingCouchRenderer = GameObject.Find("Livingroom_Couch").GetComponent<SpriteRenderer>();
+        livingTableRenderer = GameObject.Find("Livingroom_Table").GetComponent<SpriteRenderer>();
+        livingRenderer = GameObject.Find("Livingroom_Background").GetComponent<SpriteRenderer>();
+    }
+
     public void switchKitchenBool() {
         kitchenLit = !kitchenLit;
     }
 
     public void switchLivingBool() {
         livingLit = !livingLit;
+        if (livingLit == false) {
+            livingCouchRenderer.material = diffuseMaterial;
+            livingRenderer.material = diffuseMaterial;
+            livingTableRenderer.material = diffuseMaterial;
+        } else {
+            livingCouchRenderer.material = defaultMaterial;
+            livingRenderer.material = defaultMaterial;
+            livingTableRenderer.material = defaultMaterial;
+        }
     }
 
     public void switchEntranceBool() {
         entranceLit = !entranceLit;
+        if (entranceLit == false) {
+            EntranceRenderer.material = diffuseMaterial;
+            EntranceDrawerRenderer.material = diffuseMaterial;
+        } 
+        else{
+            EntranceRenderer.material = defaultMaterial;
+            EntranceDrawerRenderer.material = defaultMaterial;
+        }
     }
 
     public void switchÖverBool() {
@@ -142,11 +173,6 @@ public class RoomLighter : MonoBehaviour {
     }
 
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            if (entranceLit == true) {
-                print("it works");
-            }
-            changeRoomLightning();
-        }
+        changeRoomLightning();
     }
 }
