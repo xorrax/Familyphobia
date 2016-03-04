e11216de452367e026d9a020b62d70445e4d7131
Pothorian <pothorian@gmail.com>
2016-03-04 12:02:10 +0100

    Ögonrörelse för gräsklippare
    Påbörjad skript för ögonrörelse på gräsklipparen, den ska kunna följa
    efter Jack då han rör sig i X-led. Nuvarande problem dock är att
    matematiken är helt skev enligt unity så multiplikation krävs på
    procentsatsen.

---
+++ ExempleScene v0.1/Assets/Scripts/Effects/EyeMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeMovement : MonoBehaviour {
+    Transform jack;
+    SpriteRenderer lawnmower;
+    SpriteRenderer background;
+    Vector3 lawnmowerMin;
+    Vector3 lawnmowerMax;
+    Vector3 backgroundMin;
+    Vector3 backgroundMax;
+    Vector3 realMin;
+    Vector3 realMax;
+    float procentX;
+
+    void Start() {
+        jack = GameObject.Find("Jack").GetComponent<Transform>();
+        background = GameObject.Find("Entrance_Background").GetComponent<SpriteRenderer>();
+        lawnmower = transform.GetComponentInParent<SpriteRenderer>();
+        lawnmowerMin = lawnmower.bounds.min;
+        lawnmowerMax = lawnmower.bounds.max;
+        backgroundMin = background.bounds.min;
+        backgroundMax = background.bounds.max;
+
+        procentX = ((lawnmowerMax.x - lawnmowerMin.x) / (backgroundMax.x - backgroundMin.x)) * 4;
+    }
+    void eyeXMovement() {
+        transform.position = new Vector3(jack.transform.position.x * procentX, 0, -1);
+    }
+
+	void Update () {
+        eyeXMovement();
+	}
+}
