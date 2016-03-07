f7f6c9871a7ebf77574e96d17435895f808ad4f6
Pothorian <pothorian@gmail.com>
2016-03-07 14:57:51 +0100

    SaveLoad uppdatering
    SaveLoad prototyp 1, lista med objekt.

--- ExempleScene v0.1/Assets/Scripts/SaveLoad/SaveLoadFunc.cs
+++ ExempleScene v0.1/Assets/Scripts/SaveLoad/SaveLoadFunc.cs
@@ -5,7 +5,24 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 public class SaveLoadFunc{
 
-    public static List<Game> savedGames = new List<Game>();
+    public static List<object> saveList = new List<object>();
 
+    public static void Save() {
+        saveList.Add(Inventory.invInstance.itemList);
+        saveList.Add(Inventory.invInstance.existingItem);
 
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/saveList.gd");
+        bf.Serialize(file, SaveLoadFunc.saveList);
+        file.Close();
+    }
+
+    public static void Load() {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            SaveLoadFunc.saveList = (List<object>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
 }
