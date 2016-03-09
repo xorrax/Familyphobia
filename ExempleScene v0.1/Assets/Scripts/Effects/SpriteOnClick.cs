17d7af0ebf746d138d539dd35e9da75dce35b1f7
Pothorian <pothorian@gmail.com>
2016-03-09 11:34:16 +0100

    Ljud + kolliderförskjutning spriteOnClick
    Ändringar för spritebyte vid klickande på dem. Ljud och
    kolliderförskjutning finns nu med.

--- ExempleScene v0.1/Assets/Scripts/Effects/SpriteOnClick.cs
+++ ExempleScene v0.1/Assets/Scripts/Effects/SpriteOnClick.cs
@@ -5,17 +5,29 @@ public class SpriteOnClick : MonoBehaviour {
     public Sprite nextSprite;
     public Sprite firstSprite;
     SpriteRenderer renderer;
+    private AudioSource audioSource;
+    public AudioClip soundEffectOpen;
+    private BoxCollider boxCollider;
+    //public AudioClip soundEffectClose;
     void Start() {
         renderer = this.gameObject.GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+        boxCollider = this.gameObject.GetComponent<BoxCollider>();
         firstSprite = renderer.sprite;
     }
 
     void spriteOnClick() {
         if (renderer.sprite != nextSprite) {
             renderer.sprite = nextSprite;
+            boxCollider.center = new Vector3(boxCollider.center.x + 2, boxCollider.center.y, boxCollider.center.z);
+            //audioSource.clip = soundEffectOpen;
         } else {
             renderer.sprite = firstSprite;
+            boxCollider.center = new Vector3(boxCollider.center.x - 2, boxCollider.center.y, boxCollider.center.z);
+            //audioSource.clip = soundEffectClose;
         }
+        audioSource.clip = soundEffectOpen;
+        audioSource.Play();
     }
 
     void OnMouseOver() {
